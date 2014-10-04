8060ad5caa30afae7d6e99e7026fd579c3ba4b5f
yass8n <yass8n@yahoo.com>
2014-10-04 01:03:33 -0700

    removed trailing white space

--- p3/app/controllers/wladder_controller.rb
+++ p3/app/controllers/wladder_controller.rb
@@ -14,11 +14,12 @@ load "#{Rails.root}/lib/words4.rb"
 	def show
 		@step0 = @@step0
 		@goal = @@goal
-		@step1 = params[:step1]
-		@step2 = params[:step2]
-		@step3 = params[:step3]
-		@step4 = params[:step4]
-		@step5 = params[:step5]
+		#strip getting rid of trailing white space
+		@step1 = params[:step1].strip
+		@step2 = params[:step2].strip
+		@step3 = params[:step3].strip
+		@step4 = params[:step4].strip
+		@step5 = params[:step5].strip
 		#getting rid of empty values in the form
 		@steps_array = [@step1, @step2, @step3, @step4, @step5].reject! do |steps| steps.empty? end 
 		if (legal(@steps_array) && edit_distance_of_one(@step0, @steps_array[0]) && edit_distance_of_one(@goal, @steps_array[@steps_array.length-1]))
