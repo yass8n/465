05f0d0e79f0a3732414f686870b2f597382babee
yass8n <yass8n@yahoo.com>
2014-10-07 12:02:11 -0700

    fixed bug where 5 words causes nil array

--- p3/app/controllers/wladder_controller.rb
+++ p3/app/controllers/wladder_controller.rb
@@ -3,10 +3,11 @@ class WladderController < ApplicationController
 load "#{Rails.root}/lib/ladder.rb"
 load "#{Rails.root}/lib/words4.rb"
 
+
 	def index
+		@step0 = @@step0 = dictionary.sample
+		@goal = @@goal = dictionary.sample
 		#setting class variable and instance variable  so we can get the same value later in the 'show' function
-		@@step0 = @step0= dictionary.sample 
-		@@goal = @goal = dictionary.sample
 		while(@goal == @step0) do
 			@goal = dictionary.sample
 		end
@@ -21,7 +22,8 @@ load "#{Rails.root}/lib/words4.rb"
 		@step4 = params[:step4].strip
 		@step5 = params[:step5].strip
 		#getting rid of empty values in the form
-		@steps_array = [@step1, @step2, @step3, @step4, @step5].reject! do |steps| steps.empty? end 
+		@steps_array = [@step1, @step2, @step3, @step4, @step5]
+		@steps_array.reject! do |step| step.empty? end 
 		if (legal(@steps_array) && edit_distance_of_one(@step0, @steps_array[0]) && edit_distance_of_one(@goal, @steps_array[@steps_array.length-1]))
 			@result = "YOU WIN!"
 		else
