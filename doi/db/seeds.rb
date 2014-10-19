20d7ad758be7f695d0e3b77bf4ed190e9bd61653
yass8n <yass8n@yahoo.com>
2014-10-19 03:34:52 -0700

    done

--- doi/db/seeds.rb
+++ doi/db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+dois = DoiObject.create(
+  [
+    { name: 'google', description: 'The best search Engine EVER. Bing sucks compared to google.', doi: '3Hv1Rzqnn5aRwV-WH-TM-w'},
+    { name: 'facebook', description: 'Best Social Media Site', doi: '5KL3Vzqqq5FcwV-WX-IL-p'} 
+  ]
+)
+
+url = UrlObject.create(
+  [
+    { url: 'https://google.com', doi_object_id: dois[0].id},
+    { url: 'https://thefacebook.com', doi_object_id: dois[1].id},
+    { url: 'https://facebook.com', doi_object_id: dois[1].id}
+  ]
+)
