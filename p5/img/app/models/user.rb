fe4652a954fae4e83a18009c441b98f72c9df46c
yass8n <yass8n@yahoo.com>
2014-11-07 22:53:06 -0800

    user cant sign-in without name now

--- p5/img/app/models/user.rb
+++ p5/img/app/models/user.rb
@@ -1,6 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  validates_uniqueness_of :name
+  validates :name, presence: true
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+
 end
--- p5/img/db/schema.rb
+++ p5/img/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141108053300) do
+ActiveRecord::Schema.define(version: 20141108055408) do
 
   create_table "image_users", force: true do |t|
     t.integer  "image_id"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20141108053300) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
