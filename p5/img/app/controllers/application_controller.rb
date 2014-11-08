7f03975c39a9341f9cb3d7fcaa8be72ca636f41c
yass8n <yass8n@yahoo.com>
2014-11-08 04:48:46 -0800

    only allowing users to edit photos or delete photos if they own them...FIXED

--- p5/img/app/controllers/application_controller.rb
+++ p5/img/app/controllers/application_controller.rb
@@ -3,9 +3,23 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_filter :authenticate_user!
+  before_filter :authenticate!
   before_filter :configure_devise_permitted_parameters, if: :devise_controller?
 
   protected
+	def authenticate!
+		if params[:controller] == 'images' && (params[:action] == 'edit' || params[:action] == 'update' || params[:action] == 'destroy')
+		  current_image = Image.find(params[:id])
+
+		  if current_image.user == current_user
+		    # when you return from authentication!, the program continues to requested page
+		    return
+		  else
+		    # by redirecting here, we are preventing the user from visiting the requested page
+		    redirect_to root_url, notice: "Record not found"
+		  end
+		end
+	end
 
 	  def configure_devise_permitted_parameters
 	    registration_params = [:name, :email, :password, :password_confirmation]
