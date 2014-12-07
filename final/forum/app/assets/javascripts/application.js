ca4a14e6cf1ed7cbc1a05002ccbd5d4eb949ea86
yass8n <yass8n@yahoo.com>
2014-12-06 21:35:30 -0800

    added jquery tooltip for filters

--- final/forum/app/assets/javascripts/application.js
+++ final/forum/app/assets/javascripts/application.js
@@ -98,6 +98,26 @@ function initPage() {
   	$('#answered_true, #answered_false, #filter_rating, #filter_views, #filter_recent, #filter_oldest').on('click', function(){ 
 		set_filter_image();
     });
+
+    // Tooltip only Text
+	$('#filter-image').hover(function(){
+		        // Hover over code
+		        var title = $(this).attr('title');
+		        $(this).data('tipText', title).removeAttr('title');
+		        $('<p class="tooltip"></p>')
+		        .text(title)
+		        .appendTo('body')
+		        .fadeIn('slow');
+		}, function() {
+		        // Hover out code
+		        $(this).attr('title', $(this).data('tipText'));
+		        $('.tooltip').remove();
+		}).mousemove(function(e) {
+		        var mousex = e.pageX + 20; //Get X coordinates
+		        var mousey = e.pageY + 10; //Get Y coordinates
+		        $('.tooltip')
+		        .css({ top: mousey, left: mousex })
+		});
     function highlight_matches(){
     	var search_query = $(location).attr('href').replace(/.*title=/, ""); //replaces everything before "title"
 		search_query = search_query.replace(/&.*/, ""); //replaces everything after '&' that is right after title
