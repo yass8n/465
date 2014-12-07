4b3430dab3b39b268cf6eb91e8eeb5dfa710cee9
yass8n <yass8n@yahoo.com>
2014-12-06 23:39:08 -0800

    more minor modifications

--- final/forum/app/assets/javascripts/application.js
+++ final/forum/app/assets/javascripts/application.js
@@ -164,8 +164,9 @@ function initPage() {
 	        .after('<p class="tooltip smalltip" id="instruction"><i class="step fi-fast-forward"></i><br><span class="float-left">Edit Account and add a paypal email to start making MONEY! <span></p>');
 	        $('#instruction').hide();
 	        setTimeout(function(){ $('#instruction').show('highlight', 1000) }, 3000);
+	        setTimeout(function(){ $('#instruction').hide('explode', 1000) }, 11000);
 	    };
 	    $('#instruction').click(function(){
-	    	$('#instruction').hide('explode', 700);
+	    	setTimeout(function(){ $('#instruction').hide('explode', 1000) }, 1000);
 	    });
 }
